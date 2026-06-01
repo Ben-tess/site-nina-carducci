2e0f78930702028bcb9a9f506e9d51701cf9eebd
Benoît Tessier <tessier.benoit28@gmail.com>
2026-06-01 11:13:55 +0200

    code final

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-    baguetteBox.run('.gallery');
+window.addEventListener('DOMContentLoaded', () => {
+    const buttonAll = document.getElementById('button-all');
+    buttonAll.click();
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.filter-bar button');
+    const gallery = document.querySelector('.gallery');
+    const items   = [...document.querySelectorAll('.gallery a')];
+
+    function filterGallery(category) {
+        gallery.innerHTML = '';
+        items.forEach(item => {
+            const imgCategory = item.querySelector('img').dataset.category;
+            const match       = category === 'all' || imgCategory === category;
+            if (match) {
+                gallery.appendChild(item);
+            }
+        });
+        baguetteBox.run('.gallery');
+    }
+    buttons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const category = e.currentTarget.dataset.category;
+
+            buttons.forEach(btn => btn.classList.remove('active'));
+            e.currentTarget.classList.add('active');
+            filterGallery(category);
+        });
+    });
+    filterGallery('all');
 });
